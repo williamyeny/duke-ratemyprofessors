682a18d26b3935e09598b99084770cb67b3cb36f
will ye <williamyeny@gmail.com>
2017-07-06 19:47:05 -0400

    changed content script to call background script

--- script.js
+++ script.js
@@ -17,19 +17,17 @@
 // });
 
 
+
+
 $(function() {
   console.log("test");
   var url = "http://search.mtvnservices.com/typeahead/suggest/?q=connel+fullenkamp+AND+schoolid_s%3A1350&siteName=rmp&fl=teacherfirstname_t+teacherlastname_t+total_number_of_ratings_i+averageratingscore_rf+hotness_i"
-  $.getJSON(url, function( data ) {
-    // var items = [];
-    // $.each( data, function( key, val ) {
-    //   items.push( "<li id='" + key + "'>" + val + "</li>" );
-    // });
-    //
-    // $( "<ul/>", {
-    //   "class": "my-new-list",
-    //   html: items.join( "" )
-    // }).appendTo( "body" );
-    console.log(data);
+  chrome.runtime.sendMessage({
+      method: 'GET',
+      action: 'xhttp',
+      url: url,
+  }, function(responseText) {
+      alert(responseText);
+      /*Callback function to deal with the response*/
   });
 })
