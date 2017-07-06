f3affa85d8497fba5c86c77d8255fffd416a2c69
Shamikh Hossain <shamikh.hossain@duke.edu>
2017-07-06 10:21:54 -0400

    added script.js tests

---
+++ script.js
@@ -0,0 +1,34 @@
+// window.onload = function() {
+//     $("#testbutton").click(function() {
+//         $.ajax({
+//             url: 'http://search.mtvnservices.com/typeahead/suggest/?q=connel+fullenkamp+AND+schoolid_s%3A1350&siteName=rmp&fl=teacherfirstname_t+teacherlastname_t+total_number_of_ratings_i+averageratingscore_rf+hotness_i',
+//             dataType: 'json',
+            
+//             success: function(data) {
+//                 $("#results").append('all good');
+//                 alert(JSON.stringify(data));
+//             },
+//              error: function() {
+//                 $("#results").append("error");
+//                 alert('error');
+//             }
+//         });
+//     });
+// }); 
+
+
+window.onload = function() {
+    document.write('Hello world')
+}
+var url = "http://search.mtvnservices.com/typeahead/suggest/?q=connel+fullenkamp+AND+schoolid_s%3A1350&siteName=rmp&fl=teacherfirstname_t+teacherlastname_t+total_number_of_ratings_i+averageratingscore_rf+hotness_i"
+$.getJSON(url, function( data ) {
+  var items = [];
+  $.each( data, function( key, val ) {
+    items.push( "<li id='" + key + "'>" + val + "</li>" );
+  });
+ 
+  $( "<ul/>", {
+    "class": "my-new-list",
+    html: items.join( "" )
+  }).appendTo( "body" );
+});
