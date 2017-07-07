b3ba4230fe0e413d1a83c61d063f575968c20e16
will ye <williamyeny@gmail.com>
2017-07-07 16:11:07 -0400

    indentation (lol)

--- script.js
+++ script.js
@@ -5,66 +5,66 @@ $(function() {
 
   $("#ptifrmtgtframe").on("load", function() {
     console.info("iframe loaded");
-      var observer = new MutationObserver(function(mutations) {
+    var observer = new MutationObserver(function(mutations) {
 
-        mutations.forEach(function(mutation) {
-          console.info("drmp: DOM change occured in courses page")
+      mutations.forEach(function(mutation) {
+        console.info("drmp: DOM change occured in courses page")
 
-          //loop through spans with professor name(s)
-          var pspans = $("#ptifrmtgtframe").contents().find("span[id*='DU_DERIVED_SS_DESCR100_2']");
-          pspans.each(function(index) {
-            //get professor name(s)
-            var pname = $(this).text();
-            console.info("drmp: professor name: " + pname);
+        //loop through spans with professor name(s)
+        var pspans = $("#ptifrmtgtframe").contents().find("span[id*='DU_DERIVED_SS_DESCR100_2']");
+        pspans.each(function(index) {
+          //get professor name(s)
+          var pname = $(this).text();
+          console.info("drmp: professor name: " + pname);
 
-            //check if professor is already rated
-            if (!$(this).is('[rated]')) {
-              //modify professor name(s)
+          //check if professor is already rated
+          if (!$(this).is('[rated]')) {
+            //modify professor name(s)
 
-              var url = "http://search.mtvnservices.com/typeahead/suggest/?q=connel+fullenkamp+AND+schoolid_s%3A1350&siteName=rmp&fl=teacherfirstname_t+teacherlastname_t+total_number_of_ratings_i+averageratingscore_rf"
-              chrome.runtime.sendMessage({
-                  method: 'GET',
-                  action: 'xhttp',
-                  url: url,
-              }, function(responseText) {
-                  console.log(responseText);
-              });
-              //tag element as "rated" so it does not infinite loop when a rating is added
-              $(this).attr("rated", "");
-              $(this).append(" W O O H O O ");
-            } else {
-              console.info("drmp: there\'s already a rating!");
-            }
+            var url = "http://search.mtvnservices.com/typeahead/suggest/?q=connel+fullenkamp+AND+schoolid_s%3A1350&siteName=rmp&fl=teacherfirstname_t+teacherlastname_t+total_number_of_ratings_i+averageratingscore_rf"
+            chrome.runtime.sendMessage({
+                method: 'GET',
+                action: 'xhttp',
+                url: url,
+            }, function(responseText) {
+                console.log(responseText);
+            });
+            //tag element as "rated" so it does not infinite loop when a rating is added
+            $(this).attr("rated", "");
+            $(this).append(" W O O H O O ");
+          } else {
+            console.info("drmp: there\'s already a rating!");
+          }
 
-          });
-
-          //prevent duplicate mutation detections
-          return false;
         });
+
+        //prevent duplicate mutation detections
+        return false;
       });
-
-      var targetNode = $("#ptifrmtgtframe").contents().find("body")[0]; //checking for changes in iframe
-      var observerConfig = {
-        childList: true, // detects if elements are added or removed
-        subtree: true //detects all elements in body, not just direct children
-      };
-      observer.observe(targetNode, observerConfig);
-
-      // console.log("iframe loaded");
-      // $(document.getElementById("ptifrmtgtframe").contentDocument).on("click", "a", function() {
-      //   console.log("iframe a clicked");
-      // })
-      // $("#ptifrmtgtframe").contents().find("a[id*='DU_SEARCH_WRK_SSR_EXPAND_COLLAP2']").on('click', function() {
-        // var url = "http://search.mtvnservices.com/typeahead/suggest/?q=connel+fullenkamp+AND+schoolid_s%3A1350&siteName=rmp&fl=teacherfirstname_t+teacherlastname_t+total_number_of_ratings_i+averageratingscore_rf+hotness_i"
-        // chrome.runtime.sendMessage({
-        //     method: 'GET',
-        //     action: 'xhttp',
-        //     url: url,
-        // }, function(responseText) {
-            // console.log(responseText);
-        // });
-      // });
     });
+
+    var targetNode = $("#ptifrmtgtframe").contents().find("body")[0]; //checking for changes in iframe
+    var observerConfig = {
+      childList: true, // detects if elements are added or removed
+      subtree: true //detects all elements in body, not just direct children
+    };
+    observer.observe(targetNode, observerConfig);
+
+    // console.log("iframe loaded");
+    // $(document.getElementById("ptifrmtgtframe").contentDocument).on("click", "a", function() {
+    //   console.log("iframe a clicked");
+    // })
+    // $("#ptifrmtgtframe").contents().find("a[id*='DU_SEARCH_WRK_SSR_EXPAND_COLLAP2']").on('click', function() {
+      // var url = "http://search.mtvnservices.com/typeahead/suggest/?q=connel+fullenkamp+AND+schoolid_s%3A1350&siteName=rmp&fl=teacherfirstname_t+teacherlastname_t+total_number_of_ratings_i+averageratingscore_rf+hotness_i"
+      // chrome.runtime.sendMessage({
+      //     method: 'GET',
+      //     action: 'xhttp',
+      //     url: url,
+      // }, function(responseText) {
+          // console.log(responseText);
+      // });
+    // });
+  });
 
 
 
