99f482c842896c52e196b4d190c5c1f2e5f3281b
will ye <williamyeny@gmail.com>
2017-07-07 16:17:05 -0400

    removed outdated comments

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-//INSTRUCTOR NAME IDS TAKE THIS FORM: DU_DERIVED_SS_DESCR100_2$[NUMBER]
-
 $(function() {
   console.info("drmp: duke rmp loaded");
 
@@ -50,22 +48,6 @@ $(function() {
     };
     observer.observe(targetNode, observerConfig);
 
-    // console.log("iframe loaded");
-    // $(document.getElementById("ptifrmtgtframe").contentDocument).on("click", "a", function() {
-    //   console.log("iframe a clicked");
-    // })
-    // $("#ptifrmtgtframe").contents().find("a[id*='DU_SEARCH_WRK_SSR_EXPAND_COLLAP2']").on('click', function() {
-      // var url = "http://search.mtvnservices.com/typeahead/suggest/?q=connel+fullenkamp+AND+schoolid_s%3A1350&siteName=rmp&fl=teacherfirstname_t+teacherlastname_t+total_number_of_ratings_i+averageratingscore_rf+hotness_i"
-      // chrome.runtime.sendMessage({
-      //     method: 'GET',
-      //     action: 'xhttp',
-      //     url: url,
-      // }, function(responseText) {
-          // console.log(responseText);
-      // });
-    // });
   });
-
-
 
 })
