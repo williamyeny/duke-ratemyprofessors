89ecd7ec7eb9e45bd52a9f8ce41b6b11865a6f30
will ye <williamyeny@gmail.com>
2017-07-06 23:51:29 -0400

    bullshit code that doesnt work jfc

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
-
+//INSTRUCTOR NAME IDS TAKE THIS FORM: DU_DERIVED_SS_DESCR100_2$[NUMBER]
 
 $(function() {
-  console.log("test");
-  var url = "http://search.mtvnservices.com/typeahead/suggest/?q=connel+fullenkamp+AND+schoolid_s%3A1350&siteName=rmp&fl=teacherfirstname_t+teacherlastname_t+total_number_of_ratings_i+averageratingscore_rf+hotness_i"
-  chrome.runtime.sendMessage({
-      method: 'GET',
-      action: 'xhttp',
-      url: url,
-  }, function(responseText) {
-      console.log(responseText);
+  console.log("duke-ratemyprofessors extension loaded 2.1");
+  $("a[id*='DU_SEARCH_WRK_SSR_EXPAND_COLLAP2']").click(function() {
+    console.log("thingy clicked");
   });
+
+  $("#ptifrmtgtframe").on("load", function() {
+    console.log("iframe loaded");
+    $("#ptifrmtgtframe").contents().find("a[id*='DU_SEARCH_WRK_SSR_EXPAND_COLLAP2']").on('click', function() {
+      var url = "http://search.mtvnservices.com/typeahead/suggest/?q=connel+fullenkamp+AND+schoolid_s%3A1350&siteName=rmp&fl=teacherfirstname_t+teacherlastname_t+total_number_of_ratings_i+averageratingscore_rf+hotness_i"
+      chrome.runtime.sendMessage({
+          method: 'GET',
+          action: 'xhttp',
+          url: url,
+      }, function(responseText) {
+          console.log(responseText);
+      });
+    });
+  });
+
+
+
 })
