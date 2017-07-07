a371f94f50d47861c27071d264c069df5e8245e0
will ye <williamyeny@gmail.com>
2017-07-07 16:09:53 -0400

    remove unnecessary config options and statements
    also reformatted + commented

--- script.js
+++ script.js
@@ -3,63 +3,51 @@
 $(function() {
   console.info("drmp: duke rmp loaded");
 
-
-
   $("#ptifrmtgtframe").on("load", function() {
     console.info("iframe loaded");
-      var targetNode = $("#ptifrmtgtframe").contents().find("body")[0];
       var observer = new MutationObserver(function(mutations) {
 
         mutations.forEach(function(mutation) {
-          //check if mutation added elements
-          if (mutation.type === "childList") {
-            console.info("drmp: DOM change occured in courses page")
+          console.info("drmp: DOM change occured in courses page")
 
-            //loop through spans with professor name(s)
-            var pspans = $("#ptifrmtgtframe").contents().find("span[id*='DU_DERIVED_SS_DESCR100_2']");
-            pspans.each(function(index) {
-              //get professor name(s)
-              var pname = $(this).text();
-              console.info("drmp: professor name: " + pname);
+          //loop through spans with professor name(s)
+          var pspans = $("#ptifrmtgtframe").contents().find("span[id*='DU_DERIVED_SS_DESCR100_2']");
+          pspans.each(function(index) {
+            //get professor name(s)
+            var pname = $(this).text();
+            console.info("drmp: professor name: " + pname);
 
-              //check if professor is already rated
-              if (!$(this).is('[rated]')) {
-                //modify professor name(s)
+            //check if professor is already rated
+            if (!$(this).is('[rated]')) {
+              //modify professor name(s)
 
-                var url = "http://search.mtvnservices.com/typeahead/suggest/?q=connel+fullenkamp+AND+schoolid_s%3A1350&siteName=rmp&fl=teacherfirstname_t+teacherlastname_t+total_number_of_ratings_i+averageratingscore_rf"
-                chrome.runtime.sendMessage({
-                    method: 'GET',
-                    action: 'xhttp',
-                    url: url,
-                }, function(responseText) {
-                    console.log(responseText);
-                });
-                //tag element as "rated" so it does not infinite loop when a rating is added
-                $(this).attr("rated", "");
-                $(this).append(" W O O H O O ");
-              } else {
-                console.info("drmp: there\'s already a rating!");
-              }
+              var url = "http://search.mtvnservices.com/typeahead/suggest/?q=connel+fullenkamp+AND+schoolid_s%3A1350&siteName=rmp&fl=teacherfirstname_t+teacherlastname_t+total_number_of_ratings_i+averageratingscore_rf"
+              chrome.runtime.sendMessage({
+                  method: 'GET',
+                  action: 'xhttp',
+                  url: url,
+              }, function(responseText) {
+                  console.log(responseText);
+              });
+              //tag element as "rated" so it does not infinite loop when a rating is added
+              $(this).attr("rated", "");
+              $(this).append(" W O O H O O ");
+            } else {
+              console.info("drmp: there\'s already a rating!");
+            }
 
-            });
+          });
 
-            //prevent duplicate mutation detections
-            return false;
-
-
-          }
-
+          //prevent duplicate mutation detections
+          return false;
         });
       });
 
-      // Notify me of everything!
+      var targetNode = $("#ptifrmtgtframe").contents().find("body")[0]; //checking for changes in iframe
       var observerConfig = {
-        attributes: true,
-        childList: true,
-        characterData: true,
-        subtree: true
+        childList: true, // detects if elements are added or removed
+        subtree: true //detects all elements in body, not just direct children
       };
-      console.log(targetNode);
       observer.observe(targetNode, observerConfig);
 
       // console.log("iframe loaded");
